1acc544f4993a129d4db1fa8532da32ca5214afe
mubasharmustafa <>
2023-02-23 18:51:16 +0500

    mubashar

--- resources/views/pro/auth-register.blade.php
+++ resources/views/pro/auth-register.blade.php
@@ -217,7 +217,7 @@
                                             <div class="input-group-prepend">
                                                 <span class="input-group-text" id="basic-addon1"><i class="mdi mdi-cards font-16"></i></span>
                                             </div>
-                                            <input type="file" class="form-control" id="idCard" name="idCard">
+                                            <input type="file" id="files1" class="form-control" id="idCard" name="idCard">
                                         </div>                                    
                                     </div>
 
@@ -344,6 +344,26 @@
                         src : e.target.result,
                         title : file.name
                     }).insertAfter("#files");
+               });
+               fileReader.readAsDataURL(f);
+           }
+      });
+     } else { alert("Your browser doesn't support to File API") }
+
+     if(window.File && window.FileList && window.FileReader) {
+        $("#files1").on("change",function(e) {
+            var files = e.target.files ,
+            filesLength = files.length ;
+            for (var i = 0; i < filesLength ; i++) {
+                var f = files[i]
+                var fileReader = new FileReader();
+                fileReader.onload = (function(e) {
+                    var file = e.target;
+                    $("<img></img>",{
+                        class : "imageThumb",
+                        src : e.target.result,
+                        title : file.name
+                    }).insertAfter("#files1");
                });
                fileReader.readAsDataURL(f);
            }
